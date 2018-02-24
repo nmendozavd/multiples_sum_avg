2a72ee0bb8819f3a24aeefaca8854f392f869572
Noel Mendoza <nmendozavd@gmail.com>
2018-02-23 19:01:18 -0800

    initial commit

---
+++ multiples_sum_average.py
@@ -0,0 +1,13 @@
+#print all odds from 1 to 1000
+for i in range(1, 101, 2): print i
+
+#print all multiples of 5, from 5 to 1,000,000.
+for i in range(0, 1000000, 5): print(i)
+
+#Print sum of all values in list a = [1, 2, 5, 10, 255, 3]
+a = [1, 2, 5, 10, 255, 3]
+print sum(a)
+
+#Print average of all values in list a = [1, 2, 5, 10, 255, 3]
+a = [1, 2, 5, 10, 255, 3]
+print sum(a)/len(a)
